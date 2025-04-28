e3f8c1f82c4596ea6d9ac0d1dcaed6aa705d7445
Connor Kress <ckress@ufl.edu>
2025-04-28 03:28:37 -0400

    fixed update_database imports

--- update_database.py
+++ update_database.py
@@ -1,16 +1,17 @@
-from constants import DEST_DIR
+from constants import VIDEOS_DIR
 from database import get_all_videos, insert_video
 from util import read_metadata
 
 
 def main() -> None:
     visited_urls = set[str]()
-    for path in DEST_DIR.rglob('*.mkv'):
+    for path in VIDEOS_DIR.rglob('*.mkv'):
         print(path)
         metadata = read_metadata(path)
         if metadata is None:
             print(f"ERROR reading metadata: {path}")
             continue
+        # TODO: check if it moved before inserting it
         insert_video(path, metadata)
         visited_urls.add(metadata.url)
 
@@ -18,7 +19,7 @@ def main() -> None:
     for path, metadata in videos:
         if metadata.url not in visited_urls:
             print(f"Deleted: {metadata}")
-            # Remove from database
+            # TODO: Remove from database
 
 
 if __name__ == '__main__':
