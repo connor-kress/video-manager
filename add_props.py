1c1350a060d1a19eaa885e91d0404587a755af67
Connor Kress <ckress@ufl.edu>
2025-08-29 16:30:20 -0400

    update add_props script

--- add_props.py
+++ add_props.py
@@ -1,11 +1,18 @@
 from sys import argv, exit
 from pathlib import Path
 
+from config import load_config
+from constants import CONFIG_PATH
 from database import Metadata
 from download import set_props
+from models import LinkType
 
 
 def main() -> None:
+    config = load_config(CONFIG_PATH)
+    if config is None:
+        exit(1)
+
     print(f"{argv=}")
     if len(argv) != 6:  # 5 + 1 for program name
         print("Invalid number of args")
@@ -21,7 +28,7 @@ def main() -> None:
     in_path = Path(in_arg)
     out_path = Path(out_arg)
 
-    set_props(in_path, out_path, metadata)
+    set_props(in_path, out_path, metadata, LinkType.DEFAULT, config)
 
 
 if __name__ == "__main__":
